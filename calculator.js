d590cb18f56c23f7a8ea1d4736cd53a7be482bd4
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-07-13 17:19:20 +0900

    Write the logic for operate()

--- calculator.js
+++ calculator.js
@@ -18,5 +18,18 @@ function divide(a, b) {
 }
 
 function operate(a, b, operator) {
-
+    switch (operator) {
+        case "+":
+            add(a, b);
+            break;
+        case "-":
+            subtract(a, b);
+            break;
+        case "*":
+            multiply(a, b);
+            break;
+        case "/":
+            divide(a, b);
+            break;
+    }
 }
